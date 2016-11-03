9a0bb2f8b8d780ee8273e145289261df043e3a1e
Jonathan Wade <jwade005@seattlecentral.edu>
2016-11-02 20:50:58 -0700

    Uodate server_alert

--- server_alert.sh
+++ server_alert.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo -e "Server Alert: `hostname`:\n `uptime -p` \n USERS: `who` " | mail -s "Server Alert! `hostname`" wadejs@icloud.com
+var=$(cat /proc/uptime | cut -d " " -f1)
+users=$(/usr/bin/who | grep -c "")
+
+if [ "$var" > "720" ]; then
+    echo "Server `hostname` has been up for more than 2 hours!"
+    echo "Number of users logged on: $users"
+fi
+        if [ "$users" -ge "1" ]; then
+            echo "There are users logged on."
+
+        else
+            echo "There are no users logged on."
+fi
