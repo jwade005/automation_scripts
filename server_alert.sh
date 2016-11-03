5587e8b010d03eeef0c04dba5786139bc1d65d85
Jonathan Wade <jwade005@seattlecentral.edu>
2016-11-02 20:52:50 -0700

    Update server_alert.sh

--- server_alert.sh
+++ server_alert.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 var=$(cat /proc/uptime | cut -d " " -f1)
@@ -13,3 +14,16 @@ fi
         else
             echo "There are no users logged on."
 fi
+
+##Crontab entry to run script every 30 minutes:
+
+30 * * * * /home/ec2-user/server_alert3.sh | mail -s "Server Alert" wadejs@icloud.com
+
+##Output to email wadejs@icloud.com:
+
+##Server ip-172-31-17-171.us-west-2.compute.internal has been up for more than 2 hours!
+##Number of users logged on: 0
+##There are no users logged on.
+##Server Alert: ip-172-31-17-171.us-west-2.compute.internal:
+##up 4 hours, 38 minutes 
+##USERS:  
