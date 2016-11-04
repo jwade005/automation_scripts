903cc43d2140fcd6035d4db6c0c18efd8e216758
Jonathan Wade <jwade005@seattlecentral.edu>
2016-11-04 15:53:45 -0700

    Update dirty_cow_patch.sh
    Corrected code to run on my AWS instances.

--- dirty_cow_patch_remote.sh
+++ dirty_cow_patch_remote.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in $( cat my_machines ); do ssh -t -i "load-ballancer-static-content.pem" ec2-user@$i "sudo yum clean all && sudo yum update kernel && sudo reboot"; done
+for i in $( cat my_machines ); do ssh -t -i "RedHat7-1.pem" ec2-user@$i "sudo yum clean all && sudo yum update kernel && sudo reboot"; done
