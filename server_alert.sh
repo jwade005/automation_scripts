1ea6fe9c79dd756be407dee443dc6aeff21e6aad
Jonathan Wade <jwade005@seattlecentral.edu>
2016-12-07 17:13:16 -0800

    Updated sever_alert.sh
    Changed variable name and added exit 0

--- server_alert.sh
+++ server_alert.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-var=$(cat /proc/uptime | cut -d " " -f1)
+timer=$(cat /proc/uptime | cut -d " " -f1)
 users=$(/usr/bin/who | grep -c "")
 
-if [ "$var" > "7200" ]; then
+if [ "$timer" > "7200" ]; then
     echo "Server `hostname` Server ALERT!"
     echo "Number of users logged on: $users"
     echo -e "Server Alert: `hostname`:\n `uptime -p` \n USERS: `who` "
-fi
-        if [ "$users" -ge "1" ]; then
-            echo "There are users logged on."
 
-        else
-            echo "There are no users logged on."
+else
+    exit 0
+
+fi
+
+if [ "$users" -ge "1" ]; then
+    echo "There are users logged on."
+
+else
+    echo "There are no users logged on."
+
 fi
 
 ##Crontab entry to run script every 30 minutes:
